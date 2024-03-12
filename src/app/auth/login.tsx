261854b62601f3f653c897fc6cda45f79f05e3b4
Glency Tirao <glncy@Glencys-MacBook-Pro.local>
2024-03-12 15:12:55 +0800

    chore: pressable press not working when using nativewind

--- src/app/auth/login.tsx
+++ src/app/auth/login.tsx
@@ -1,18 +1,19 @@
 import { useRouter } from "expo-router";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 
-import { Button } from "@/src/components/reusables/ui/button";
 import { Text } from "@/src/components/reusables/ui/text";
 
 export default function LoginScreen() {
   const router = useRouter();
-
   return (
     <View>
-      <Text>Login</Text>
-      <Button onPress={() => router.push("/auth/signup")}>
-        <Text>Signup</Text>
-      </Button>
+      <Pressable
+        style={({ pressed }) => [{ backgroundColor: pressed ? "blue" : "red" }]}
+        className="w-full p-5"
+        onPress={() => router.push("/auth/signup")}
+      >
+        <Text className="text-center">Press me</Text>
+      </Pressable>
     </View>
   );
 }
